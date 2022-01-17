229d554838324253e164563a2a8f9f82c42eda46
Anais Moutarlier <anaismoutarlier@gmail.com>
2022-01-17 12:09:21 +0100

    function hello init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react'
+
 function App() {
+  const [text, setText] = useState("hello world !")
+
+  const getMessage = async () => {
+    const data = await fetch('/.netlify/functions/hello')
+    const json = await data.json()
+    
+    const { message } = json
+    setText(message)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { text }
+        <button onClick={ getMessage }>CLICK</button>
       </header>
     </div>
   );
